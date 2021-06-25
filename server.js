2e7fad1aa8dd4ea6a935fc716211d70424353c81
bjaworski2002 <bjaworski2002@gmail.com>
2021-06-25 14:26:44 +0200

    Archive Upload (02.2021) Debug.2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const util = require("util");
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const sendHeaders = (res, contentType, statusCode, kutas) => {
     console.log(contentType)
